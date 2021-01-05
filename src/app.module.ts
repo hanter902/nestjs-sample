6cb7cd9efba7b0da36783cbe2380425223d755cc
Xuân Đặng <dangthanhxuan902@gmail.com>
2021-01-05 11:54:56 +0700

    regist account

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
     imports: [
         TypeOrmModule.forRoot(typeOrmConfig),
-        TasksModule
+        TasksModule,
+        AuthModule
     ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule { }
