6441632036dad8fc3017b680cf72341b15261b76
Xuân Đặng <dangthanhxuan902@gmail.com>
2021-01-13 16:14:30 +0700

    authorize api task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -23,8 +23,8 @@ export class TasksController {
     }
 
     @Get('/:id')
-    async getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
-        return this.tasksService.getTaskById(id);
+    async getTaskById(@Param('id', ParseIntPipe) id: number, @GetUser() user: User): Promise<Task> {
+        return this.tasksService.getTaskById(id, user);
     }
 
     @Post()
@@ -34,12 +34,12 @@ export class TasksController {
     }
 
     @Delete('/:id')
-    async deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
-        return this.tasksService.deleteTask(id);
+    async deleteTask(@Param('id', ParseIntPipe) id: number, @GetUser() user: User): Promise<void> {
+        return this.tasksService.deleteTask(id, user);
     }
 
     @Put('/:id/status')
-    async updateTaskStatus(@Param('id', ParseIntPipe) id: number, @Body('status', TaskStatusValidationPipe) status: TaskStatus) {
-        return this.tasksService.updateTaskStatus(id, status);
+    async updateTaskStatus(@Param('id', ParseIntPipe) id: number, @Body('status', TaskStatusValidationPipe) status: TaskStatus, @GetUser() user: User) {
+        return this.tasksService.updateTaskStatus(id, status, user);
     }
 }
