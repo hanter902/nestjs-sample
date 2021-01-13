61def3392d8121e901d37cbdbfd9ce7ca24ef570
Xuân Đặng <dangthanhxuan902@gmail.com>
2021-01-13 17:39:41 +0700

    config server

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -5,12 +5,13 @@ import { Strategy, ExtractJwt } from 'passport-jwt';
 import { JwtPayload } from './jwt-payload.interface';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
+import * as config from 'config';
 
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor(@InjectRepository(UserRepository) private userRepository: UserRepository) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: 'topSecret51'
+            secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret')
         });
     }
 
